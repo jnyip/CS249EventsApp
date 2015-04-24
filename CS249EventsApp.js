b9a739a8c411022833eb4d7721349db115544a69
priscilla-lee <plee3@wellesley.edu>
2015-04-24 16:56:03 -0400

    added threads, responses, bootstrapped stuff

--- CS249EventsApp.js
+++ CS249EventsApp.js
@@ -1,82 +1,88 @@
-// simple-todos.js
-Tasks = new Mongo.Collection("tasks");
+Threads = new Mongo.Collection("threads");
 
-if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
-  });
-    // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
-Template.body.events({
-  "submit .new-task": function(event){
-    // This function is called when the new task form is submitted
+if (Meteor.isClient) { //This code only runs on the client
+	Template.body.helpers({
+		threads: function () {
+			return Threads.find({}, {sort: {createdAt: -1}});
+		}
+	});
+	
+	Template.body.events({
+		"submit .new-thread": function(event){
+			event.preventDefault(); //prevents default form submit
+			var text = event.target.thread.value;
+			Threads.insert({
+				text: text,
+				createdAt: new Date(), // current time
+				responses: [],
+				active: true
+			});
+			event.target.thread.value = ""; //clear form
+			//return false; //prevents default form submit, but won't work if error above
+		}
+	});
+	
+	Template.oneThread.helpers({
+		responses: function() {
+			var thisThread = Threads.find(this._id).fetch();
+			return thisThread[0].responses;
+		},
+		active: function() {
+			var thisThread = Threads.find(this._id).fetch();
+			return thisThread[0].active;
+		}
+		
+	});
 
-    var text = event.target.text.value;
+	Template.oneThread.events({
+		// "click .toggle-checked": function () {
+			// // Set the checked property to the opposite of its current value
+			// Threads.update(this._id, {$set: {checked: ! this.checked}});
+		// },
+		"click .delete": function () {
+			Threads.remove(this._id);
+		},
+		"submit .new-response": function(event){
+			event.preventDefault();
+			var text = event.target.response.value;
+			Threads.update(this._id, {$push: {responses: text}});
+			event.target.response.value = "";
+		},
+		"click .done": function() {
+			Threads.update(this._id, {$set: {active: !this.active}});
+		}
+	});
 
-    Tasks.insert({
-      text: text,
-      createdAt: new Date() // current time
-    });
-
-    // Clear form
-    event.target.text.value = "";
-
-    // Prevent default form submit
-    return false;
-  },
- "submit .new-reply": function(event){
-     var value=event.target.text.value;
-     console.log(value);
-     return false;
- }
-    
-    
-});
-Template.quickHelp.events({
-  "click .toggle-checked": function () {
-    // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {$set: {checked: ! this.checked}});
-  },
-  "click .delete": function () {
-    Tasks.remove(this._id);
-  }
-});
-
-    console.log("HELLO. PRISCILLA WAS HERE");
-    Accounts.ui.config({
-        requestPermissions: {},
-        extraSignupFields: [{
-            fieldName: 'first-name',
-            fieldLabel: 'First name',
-            inputType: 'text',
-            visible: true,
-            validate: function(value, errorFunction) {
-              if (!value) {
-                errorFunction("Please write your first name");
-                return false;
-              } else {
-                return true;
-              }
-            }
-        }, {
-            fieldName: 'last-name',
-            fieldLabel: 'Last name',
-            inputType: 'text',
-            visible: true,
-        }, {
-            fieldName: 'coordinator',
-            fieldLabel: 'I am a coordinator',
-            inputType: 'checkbox',
-            visible: true,
-            saveToProfile: true
-        } ]
-    });
+	Accounts.ui.config({
+		requestPermissions: {},
+		extraSignupFields: [{
+			fieldName: 'first-name',
+			fieldLabel: 'First name',
+			inputType: 'text',
+			visible: true,
+			validate: function(value, errorFunction) {
+			  if (!value) {
+				errorFunction("Please write your first name");
+				return false;
+			  } else {
+				return true;
+			  }
+			}
+		}, {
+			fieldName: 'last-name',
+			fieldLabel: 'Last name',
+			inputType: 'text',
+			visible: true,
+		}, {
+			fieldName: 'coordinator',
+			fieldLabel: 'I am a coordinator',
+			inputType: 'checkbox',
+			visible: true,
+			saveToProfile: true
+		} ]
+	});
 }
 
-
-
 if (Meteor.isServer) {
-    
+	
 }
